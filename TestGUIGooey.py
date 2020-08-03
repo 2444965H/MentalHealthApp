30464d58765c5bf5adb07394d319d07d2ef88fee
Linh <2444965H@student.gla.ac.uk>
2020-08-03 19:56:45 +0200

    TRYING TO CONNECT KnowledgeEngine with GUI - changed GUI from radiobuttons to DropDown because clicking radiobuttons is too inaccurate

--- TestGUIGooey.py
+++ TestGUIGooey.py
@@ -11,16 +11,33 @@ import SeverityAlgorithm
 def guiMakerFinance():
     parser = GooeyParser(description="Check Financials")
 
-    group = parser.add_mutually_exclusive_group()
-    financialdistress = group.add_argument("-yes", "--yes, I'm in financial distress", action="store_true")
-    financialdistress = group.add_argument("-no", "--no, I'm not in financial distress", action="store_true")
+    #group1 = parser.add_mutually_exclusive_group()
+    #group2 = parser.add_mutually_exclusive_group()
+    group1 = parser.add_argument(
+        "financialdistress",
+        metavar='Are you in financial distress?',
+        choices=["yes, I'm in financial distress", "no, I'm not in financial distress"],
+        #default="yes, I'm in financial distress" #If you want to have one of  the values as a default value, re-comment this
+    )
 
+    group2 = parser.add_argument(
+        "employment",
+        metavar='Which type of emplyoment do you have?',
+        choices=['Full-Time', 'Part-Time', 'On sabbatical - not working by choice', 'Otherwise occupied', 'Job-seeking'])
 
-    group = parser.add_mutually_exclusive_group()
-    employment = group.add_argument("-Full-Time", "--I'm working Full-Time", action="store_true")
-    employment = group.add_argument("-Part-Time", "--I'm working Part-Time", action="store_true")
-    args = parser.parse_args()
-    print("Response:", args)
+    #financialdistress = group1.add_argument("-financiallyDistressed", metavar="--yes, I'm in financial distress", action="store_true")
+    #financialdistress = group1.add_argument("-notfinanciallyDistressed", metavar="--no, I'm not in financial distress", action="store_true")
+
+    #employment = group2.add_argument("-Full-Time", metavar="--I'm working Full-Time",  action="store_true")
+    #employment = group2.add_argument("-Part-Time", metavar="--I'm working Part-Time", action="store_true")
+
+    #parser.
+    args = parser.parse_args(args="employment")
+    #print("Response:", args) #Redo uncomment this to check what input it takes in GUI
+    return args
+
+# def testPrint():
+#     parser = GooeyParser
 
 
 #    parser.add_argument('filename', help="name of the file to process", widget='FileChooser')
--- TestKnowledgeEngine.py
+++ TestKnowledgeEngine.py
@@ -12,19 +12,21 @@ class ContextualQuestions(KnowledgeEngine):
         if (SeverityAlgorithm.Depression() >= 5) or (SeverityAlgorithm.Anxiety() >= 5) or (
                 SeverityAlgorithm.Stress() >= 5):
             yield Fact(action="DefineFinancialSituation")
+            teststring = TestGUIGooey.guiMakerFinance()
+            print(teststring)
 
     @Rule(Fact(action='DefineFinancialSituation'),
           NOT(Fact(financialdistress=W())))
     def ask_financialdistress(self, financialdistress=None):
-        financialdistress=TestGUIGooey.guiMakerFinance()
-        financialdistress= self.declare(Fact(financialdistress=TestGUIGooey.guiMakerFinance))
+        #financialdistress=TestGUIGooey.guiMakerFinance()
+        financialdistress= self.declare(Fact(financialdistress=input))
         #pass financialdistress
 
     @Rule(Fact(action='DefineFinancialSituation'),
           NOT(Fact(employment=W())))
     def ask_employment(self, employment=None):
-        employment=TestGUIGooey.guiMakerFinance()
-        self.declare(Fact(employment=TestGUIGooey.guiMakerFinance))
+        #employment=TestGUIGooey.guiMakerFinance()
+        self.declare(Fact(employment=input))
         # self.declare(Fact(employment=input("What kind of employment do you have? Type in: Full-Time or Part-Time")))
 
     # @Rule(Fact(action='DefineFinancialSituation'),
@@ -33,11 +35,11 @@ class ContextualQuestions(KnowledgeEngine):
     # def greet(self, financialdistress, employment):
     #     print("You are in financial distress: %s. Your employment status: %s" % (financialdistress, employment))
 
-    @Rule(Fact(action='DefineFinancialSituation'),
-          Fact(financialdistress='yes'),
-          Fact(employment='Full-Time'))
-    def financial_advice_1(self):
-        print("Financial Advice 1: Develop a Personal Financial Strategy")
+    # @Rule(Fact(action='DefineFinancialSituation'),
+    #       Fact(financialdistress='yes'),
+    #       Fact(employment='Full-Time'))
+    # def financial_advice_1(self):
+    #     print("Financial Advice 1: Develop a Personal Financial Strategy")
 
 
 engine = ContextualQuestions()
